cf889f5ebb6a63ceb8f49e058bb414762ca06a8b
Maksim Chernov <chernov.maksim2008@gmail.com>
2022-11-12 12:20:37 +0300

    2v

---
+++ main.py
@@ -0,0 +1,32 @@
+from tkinter import *
+import os
+#создание окон
+main = Tk() #делает окно
+main.title('банк') #заголовок окна
+main.geometry('500x500') #размер окна
+#регистрация
+def sing_up():
+    sing_up_screen = Toplevel(main)
+    sing_up_screeen.title('Sing Up')
+    main.geometry('200x200')
+
+    labes(main, text=',логин и пароль').grid(row=0) #ров это строчка
+    labes(main, text=',логин и пароль').pack(side=TOP)
+    labes(main, text=',логин и пароль').pack(side=TOP)
+
+mainloop()
+#создание кортанки
+img = Image.open()
+ing = Img.resize((150,150))
+img = ImageTk.PhotoImage(ing)
+
+
+# labels
+labes(main, text=',безопасный банк').pack(side=TOP)
+label(main, image=img).pack(side=TOP)
+
+#кнопки
+Button(main, text ='Sing Up', font=('Calibri', 12),width=15), command =sing_up).pack(side=TOP,pady=10)
+Button(main, text ='Log In', font=('Calibri', 12),width=15).pack(side=TOP)
+
+main.mainloop()
